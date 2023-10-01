a307038c704917f5643a533d464251e284baf255
phuongphamduy <92977825+phuongphamduy@users.noreply.github.com>
2023-10-01 22:33:30 +0700

    mapbox integration

--- reactjs/blockchain-app/src/components/Map/Map.js
+++ reactjs/blockchain-app/src/components/Map/Map.js
@@ -1,23 +1,47 @@
-import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
+// import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
+import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from './Map.module.scss';
-import { useMemo } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 function Map() {
-    const center = useMemo(() => {
-        return { lat: 10.81344, lng: 106.6205184 };
-    }, []);
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: 'AIzaSyAvO0DHfxQd-GNgthlZd15ACPt1DrNkwBA',
-    });
+    //Google map
 
-    if (!isLoaded) {
-        return <div>Loading...</div>;
-    }
+    // const center = useMemo(() => {
+    //     return { lat: 10.81344, lng: 106.6205184 };
+    // }, []);
+    // const { isLoaded } = useLoadScript({
+    //     googleMapsApiKey: 'AIzaSyAvO0DHfxQd-GNgthlZd15ACPt1DrNkwBA',
+    //     libraries: ['places'],
+    // });
+
+    // if (!isLoaded) {
+    //     return <div>Loading...</div>;
+    // }
+
+    // mapbox
+
+    const Map = ReactMapboxGl({
+        accessToken: 'pk.eyJ1IjoicGh1b25ncGhhbSIsImEiOiJjbG43azZsbGQwczl6MmtwOW8xZjN5ZDF1In0.X92d8OJ0kj2x4iYvkziY_A',
+    });
 
     return (
         <>
-            <GoogleMap zoom={10} center={center} mapContainerClassName={styles['map-container']}>
-                <MarkerF position={center} />
-            </GoogleMap>
+            <Map
+                // eslint-disable-next-line
+                style="mapbox://styles/mapbox/streets-v9"
+                containerStyle={{
+                    height: '100%',
+                    width: '100%',
+                }}
+                center={{ lat: 10, lng: 50 }}
+                zoom={[5]}
+            >
+                <Marker coordinates={[10, 50]} anchor="bottom">
+                    <FontAwesomeIcon icon={faLocationDot} className={styles['icon']} />
+                </Marker>
+            </Map>
+            ;
         </>
     );
 }
--- reactjs/blockchain-app/src/index.js
+++ reactjs/blockchain-app/src/index.js
@@ -9,6 +9,7 @@ import GlobalStyle from './components/GlobalStyle';
 <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>;
 <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossorigin></script>;
 <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>;
+<link href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css" rel="stylesheet" />;
 
 ReactDOM.render(
     <React.StrictMode>
