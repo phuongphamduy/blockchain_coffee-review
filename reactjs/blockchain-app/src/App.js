1d67befa5b996aea6ce7383afe67a4128285b61a
hothanhdien <103317639+hothanhdien@users.noreply.github.com>
2023-10-09 09:01:54 +0700

    1111
    1111

--- reactjs/blockchain-app/src/App.js
+++ reactjs/blockchain-app/src/App.js
@@ -7,6 +7,8 @@ import Account from './layout/components/admin/Account';
 import PostAdmin from './layout/components/admin/PostAdmin';
 import Statistic from './layout/components/admin/Statistic';
 import AllowPost from './layout/components/admin/AllowPost';
+import Login from './layout/components/Login';
+import Signup from './layout/components/Signup';
 
 function App() {
     return (
@@ -22,6 +24,14 @@ function App() {
                     <Route path="/post" element={<MainLayout />}>
                         <Route index element={<Post />} />
                         <Route path="detail" element={<PostDetail />} />
+                    </Route>
+                    <Route path="/login" element={<MainLayout />}>
+                        <Route index element={<Login />} />
+                       
+                    </Route>
+                    <Route path="/signup" element={<MainLayout />}>
+                        <Route index element={<Signup />} />
+                       
                     </Route>
                 </Routes>
             </Router>
