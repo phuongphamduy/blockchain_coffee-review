117038c3d2aa0c6918b7dbaa2384f8520ffba45c
phuongphamduy <92977825+phuongphamduy@users.noreply.github.com>
2023-10-10 11:51:02 +0700

    fix homejs

--- reactjs/blockchain-app/src/layout/components/Home/Home.js
+++ reactjs/blockchain-app/src/layout/components/Home/Home.js
@@ -1,12 +1,4 @@
-import { useLocation } from 'react-router-dom';
-
-import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from '@reach/combobox';
 import '@reach/combobox/styles.css';
-import { useEffect, useState } from 'react';
-import _ from 'lodash';
-import { setDefaults, geocode, RequestType } from 'react-geocode';
-import { useDispatch } from 'react-redux';
-import { chooseAddress } from '~/redux/coodinate';
 import backgroundImage from '~/statics/images/back_map.png';
 import styles from './Home.module.scss';
 import SearchAddress from '~/components/SearchAddress';
@@ -16,34 +8,28 @@ const Home = () => {
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: 'cover',
     };
-    
-    return (
 
+    return (
         <div className={styles['home-background']} style={divStyle}>
             <div className={styles['background-color']}>
-
                 <div className={styles['flex']}>
-                <div className={styles['intro_text']} >
-                    <p>COFFEE REVIEW</p></div>
+                    <div className={styles['intro_text']}>
+                        <p>COFFEE REVIEW</p>
+                    </div>
 
-                <div>
-                    <p className={styles['text_1']}>
-                        Xem đánh giá các quán cà phê ở khu vực Hồ Chí Minh
-                    </p>
-                </div>
-                <br />
-                <br />
-                
+                    <div>
+                        <p className={styles['text_1']}>Xem đánh giá các quán cà phê ở khu vực Hồ Chí Minh</p>
+                    </div>
+                    <br />
+                    <br />
 
-                    <div> 
-                        <SearchAddress /> 
+                    <div>
+                        <SearchAddress />
                     </div>
                 </div>
-                
             </div>
         </div>
     );
-
-    }
+};
 
 export default Home;
